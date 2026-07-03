a16978b66dc2957a5ebda4d4eab7772248bf0518
Sidharth Pattnayak <sidharthpattnayak23@gmail.com>
2026-07-03 17:42:57 +0530

    Time: 140 ms (49.04%), Space: 43 MB (59.71%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int row = board.length;
+        int col = board[0].length;
+        for(int r = 0; r< row ;r++){
+            for(int c= 0;c<col;c++){
+                if(dfs(board,word,r,c,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean dfs(char[][] board,String word,int r,int c,int index){
+        if(index==word.length()){
+            return true;
+        }
+        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length){
+               return false;
+        }
+
+        if(board[r][c]!=word.charAt(index)){
+            return false;
+        }
+                char temp = board[r][c];
+                board[r][c]='#' ;
+       // board[r][c]='#';
+
+       boolean found= 
+            dfs(board,word,r+1,c,index+1) ||
+           
+            dfs(board,word,r-1,c,index+1)||
+            
+             dfs(board,word,r,c+1,index+1)||
+                     
+                 dfs(board,word,r,c-1,index+1);
+                 
+           board[r][c]=temp;
+           return found;
+            
+    }
+}
